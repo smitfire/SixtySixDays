b577884ddc3079b704e2ab3802ccaf1b170e82aa
Nick Smit <nick.smit@gmail.com>
2014-10-13 17:28:54 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SixtySixDaysButTheBitchAintOne::Application.config.secret_key_base = '02b49dc1173897196366e9a6019be9ef5d0766adf7045777d1b724182bd8e29e69f5e2eb2a3d08faa643e2d89ac4b8fad3cab373f9cd7364c07f5c6c87dad3dc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SixtySixDaysButTheBitchAintOne::Application.config.session_store :cookie_store, key: '_SixtySixDaysButTheBitchAintOne_session'
