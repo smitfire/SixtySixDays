2ea8bf4c6d2165ad490cd7e7382a7070c6cd2fa2
Nick Smit <nick.smit@gmail.com>
2014-10-15 14:20:21 +0200

    set the session for the login and adjusted the layout home slightly

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,7 @@ class SessionsController < ApplicationController
   def create
     if User.find_by(email: params[:user][:email]) &&  User.find_by(email: params[:user][:email]).authenticate(params[:user][:password])
       @user = User.find_by(email: params[:user][:email]).authenticate(params[:user][:password])
+      session[:user_id] = @user.id
       redirect_to user_path(@user.id)
     else
       redirect_to root_path  
